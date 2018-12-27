45033219688fb06e0532f1987618a368f700f703
oleg.shilo <oleg.shilo@leicabiosystems.com>
2018-12-27 20:42:17 +1100

    - Added build.cmd

---
+++ src/build.cmd
@@ -0,0 +1,24 @@
+cd cscs.exe.core
+md "..\out\.NET Core"
+"..\out\.NET Core\css.exe" -server:stop
+dotnet publish -c Release -f netcoreapp2.1 -o "..\out\.NET Core"
+copy ..\css\bin\Release\css.exe "..\out\.NET Core\css.exe"
+cd ..\out\.NET Core
+del *.dbg
+del *.pdb
+
+echo >  -code.header //css_ac freestyle
+echo >> -code.header using System;
+echo >> -code.header using System.IO;
+echo >> -code.header using System.Reflection;
+echo >> -code.header using System.Diagnostics;
+
+echo off
+
+7z.exe a -r "..\cs-script.core.7z" "*.*"
+css -code var version = Assembly.LoadFrom(@``cscs.eng.dll``).GetName().Version.ToString();`nFile.Copy(``..\\cs-script.core.7z``, $``..\\cs-script.core.v{version}.7z``, true);
+css -code var version = Assembly.LoadFrom(@``cscs.eng.dll``).GetName().Version.ToString();`nConsole.WriteLine($`` \nBuilt: ..\\cs-script.core.v{version}.7z``);
+
+cd ..\..
+
+pause

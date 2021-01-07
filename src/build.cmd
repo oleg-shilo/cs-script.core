9f1e7abf2af2626a2ae2544cafee009637fe226a
oleg.shilo <oleg.shilo@LBSMEL-1BQHTN2>
2021-01-07 12:30:45 +1100

    - CI wip

--- src/build.cmd
+++ src/build.cmd
@@ -70,7 +70,7 @@ echo off
 copy ..\..\CSScriptLib\src\CSScriptLib\output\*.*nupkg ..\
 
 
-7z.exe a -r "..\cs-script.core.7z" "*.*"
+..\ci\7z.exe a -r "..\cs-script.core.7z" "*.*"
 .\css -code var version = Assembly.LoadFrom(#''cscs.dll#'').GetName().Version.ToString();#nFile.Copy(#''..\\cs-script.core.7z#'', $#''..\\cs-script.core.v{version}.7z#'', true);
 .\css -code var version = Assembly.LoadFrom(#''cscs.dll#'').GetName().Version.ToString();#nFile.Copy(#''..\\cs-script.core.7z#'', $#''..\\cs-script.core.v{version}.7z#'', true);
 del ..\cs-script.core.7z
